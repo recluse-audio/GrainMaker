454a24eba9748afe6f13eae5518e6d51e514a14d
Ryan Devens <ryan@antarestech.com>
2024-09-16 17:06:24 -0500

    Modify regen script to populate stuff included from RD

--- regenSource.py
+++ regenSource.py
@@ -4,17 +4,22 @@ import os
 # This is for generating a CMake environment variable via the 'set' function
 # Use this to quickly regenerate SourceFiles.cmake / TestFiles.cmake
 
-def generate_files_list(root_folder, output_file, variable_name):
+def generate_files_list(root_folders, output_file, variable_name):
     files_list = []
-    for folder, subfolders, files in os.walk(root_folder):
-        for filename in files:
-            if filename.endswith('.cpp') or filename.endswith('.h'):
-                files_list.append(os.path.join(folder, filename))
+    for root_folder in root_folders:
+        for folder, subfolders, files in os.walk(root_folder):
+            for filename in files:
+                if filename.endswith('.cpp') or filename.endswith('.h'):
+                    files_list.append(os.path.join(folder, filename))
     files_list.sort()
     files_list = "\n    ".join(files_list)
     output = 'set({}\n    {}\n)'.format(variable_name, files_list)
     with open(output_file, 'w+') as f:
         f.write(output)
 
-generate_files_list('SOURCE', 'CMAKE/SourceFiles.cmake', 'SourceFiles')
-generate_files_list('TESTS', 'CMAKE/TestFiles.cmake', 'TestFiles')
+source_folders = ['SOURCE', 'SUBMODULES/RD/SOURCE']
+generate_files_list(source_folders, 'CMAKE/SOURCES.cmake', 'SOURCES')
+
+test_folders = ['TESTS', 'SUBMODULES/RD/TESTS']
+generate_files_list(test_folders, 'CMAKE/TESTS.cmake', 'TESTS')
+
