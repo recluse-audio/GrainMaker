05d73a903fb7dbcebb2c025520b7c56aaf796f07
Ryan Devens <ryan@antarestech.com>
2024-09-26 18:01:38 -0500

    added pitch detector class

--- TESTS/test_PitchDetector.cpp
+++ TESTS/test_PitchDetector.cpp
@@ -1,3 +1,10 @@
 #include "../SOURCE/PITCH/PitchDetector.h"
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
+#include "../SOURCE/PITCH/PitchDetector.h"
+
+
+TEST_CASE("Can perfrom difference calculation")
+{
+    
+}
