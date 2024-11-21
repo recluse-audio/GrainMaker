dec53c561c2f026589dd044c90a7d4b0d058a934
Ryan Devens <ryan_devens@proton.me>
2024-11-21 14:22:39 -0600

    All tests passing: PitchMarkedCircularBuffer was breaking when marking and incrementing, not any more

--- SOURCE/PITCH/PitchMarkedCircularBuffer.h
+++ SOURCE/PITCH/PitchMarkedCircularBuffer.h
@@ -17,9 +17,16 @@
 class PitchMarkedCircularBuffer 
 {
 public:
+    enum class MarkingRule
+    {
+        kNone = 0,
+        kMagnitude = 1,
+    };
 
     PitchMarkedCircularBuffer();
     ~PitchMarkedCircularBuffer();
+
+    void prepare(double sampleRate, int maxBlockSize);
 
     // sets size of internal buffers
     void setSize(int numChannels, int numSamples);
@@ -33,13 +40,34 @@ public:
     float popBufferAndPitch(juce::AudioBuffer<float>& buffer, juce::AudioBuffer<float>& pitchBuffer);
 
     void popAudioBuffer(juce::AudioBuffer<float>& buffer);
+
+    void setMarkingRule(PitchMarkedCircularBuffer::MarkingRule newRule);
+
+    PitchMarkedCircularBuffer::MarkingRule getMarkingRule();
+
+    void checkWritePos(int& audioWritePos, int& pitchMarkWritePos);
+
 private: 
     friend class PitchBufferTest; 
+    friend class SineWavePitchBuffer;
+
+    MarkingRule mMarkingRule;
+
+    // reads audioData and accordingly marks mPitchBuffer by writing to an AudioBlock taken from its internal buffer
+    void _pitchMarkBlock(juce::dsp::AudioBlock<float> audioData, juce::dsp::AudioBlock<float> markData, int period);
+    
+    // given two buffers, this will clear and resize them to be the same size as the underlying circular buffers
+    // fills them with the corresponding audio and pitch data.  
+    // initially making this for testing purposes
+    void _loadDataIntoBuffer(juce::AudioBuffer<float>& audioBuffer, juce::AudioBuffer<float>& pitchBuffer);
+
 
     // Returns sample index in a given range of a buffer. Index is relative to 'start' argument, not buffer[0][0]
     // TODO: handle stereo 
     int _findMaxSampleIndex(juce::AudioBuffer<float>& buffer, int start, int length);
 
+    // temp buffer allocated ahead of time to do block sized markings and push into mPitchBuffer.
+    juce::AudioBuffer<float> mMarkingBuffer;
     CircularBuffer mAudioBuffer;
     CircularBuffer mPitchBuffer;
 };
